CyanogenMod/android_hardware_ril

rild: Weaken some symbols
rild: Weaken the RIL_setRilSocketName symbol

custom libril implementations won't have it, but its lack isn't
fatal outside of multi-SIM

Change-Id: If8312078921bb4ed756dcd86e17e85847638c155

rild: Unbreak multi-SIM socket name

Change-Id: Icae742c42c45726105d1a9e07a26bc4d10cc82bf

Weaken external rild symbol.

Not all libril will have this

Change-Id: I3da165f5cf779a02c7eb168d6e3c9b398d284835

rild/rild.c
@@ -48,7 +48,7 @@ static void usage(const char *argv0) {
     exit(EXIT_FAILURE);
 }
 
-extern char rild[MAX_SOCKET_NAME_LENGTH];
+extern char rild[MAX_SOCKET_NAME_LENGTH] __attribute__((weak));
 
 extern void RIL_register (const RIL_RadioFunctions *callbacks);
 
@@ -71,6 +71,7 @@ extern void RIL_onUnsolicitedResponse(int unsolResponse, const void *data,
 extern void RIL_requestTimedCallback (RIL_TimedCallback callback,
         void *param, const struct timeval *relativeTime);
 
+extern void RIL_setRilSocketName(char * s) __attribute__((weak));
 
 static struct RIL_Env s_rilEnv = {
     RIL_onRequestComplete,
@@ -177,7 +178,11 @@ int main(int argc, char **argv) {
         exit(0);
     }
     if (strncmp(clientId, "0", MAX_CLIENT_ID_LENGTH)) {
-        RIL_setRilSocketName(strncat(rild, clientId, MAX_SOCKET_NAME_LENGTH));
+        if (RIL_setRilSocketName) {
+            RIL_setRilSocketName(strncat(rild, clientId, MAX_SOCKET_NAME_LENGTH));
+        } else {
+            RLOGE("Trying to instantiate multiple rild sockets without a compatible libril!");
+        }
     }
 
     if (rilLibPath == NULL) {

SiXROM-OREO/hardware_ril

rild/rild.c
@@ -47,7 +47,7 @@ static void usage(const char *argv0) {
 }
 
 extern char ril_service_name_base[MAX_SERVICE_NAME_LENGTH];
-extern char ril_service_name[MAX_SERVICE_NAME_LENGTH];
+extern char ril_service_name[MAX_SERVICE_NAME_LENGTH] __attribute__((weak));
 
 extern void RIL_register (const RIL_RadioFunctions *callbacks);
 extern void rilc_thread_pool ();
@@ -73,6 +73,7 @@ extern void RIL_onUnsolicitedResponse(int unsolResponse, const void *data,
 extern void RIL_requestTimedCallback (RIL_TimedCallback callback,
         void *param, const struct timeval *relativeTime);
 
+extern void RIL_setRilSocketName(char * s) __attribute__((weak));
 
 static struct RIL_Env s_rilEnv = {
     RIL_onRequestComplete,
@@ -150,7 +151,11 @@ int main(int argc, char **argv) {
     if (strncmp(clientId, "0", MAX_CLIENT_ID_LENGTH)) {
         strncpy(ril_service_name, ril_service_name_base, MAX_SERVICE_NAME_LENGTH);
         strncat(ril_service_name, clientId, MAX_SERVICE_NAME_LENGTH);
-        RIL_setServiceName(ril_service_name);
+        if (RIL_setRilSocketName) {
+            RIL_setRilSocketName(ril_service_name);
+        } else {
+            RLOGE("Trying to instantiate multiple rild sockets without a compatible libril!");
+        }
     }
 
     if (rilLibPath == NULL) {